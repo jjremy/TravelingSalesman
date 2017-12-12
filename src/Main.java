ce23650fa5e21ee961cf0cbcd52130252165e464
jjremy <jeremy_kunen@student.wayland.k12.ma.us>
2017-12-12 10:44:42 -0500

    yeeti

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Population p = new Population(10);
         int score = 0;
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 10000; i++) {
             p.runGen();
             score = p.getChromosomes().get(p.getChromosomes().size() - 1).calcCost();
             //System.out.println(score);
