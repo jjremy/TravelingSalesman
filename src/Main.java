7777ed5a5e6586abc7df0d2cdf2906a1212aef91
jjremy <jeremy_kunen@student.wayland.k12.ma.us>
2017-12-13 10:16:26 -0500

    fixed cost

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,21 @@ public class Main {
     public static void main(String[] args) {
         Population p = new Population(10);
         int score = 0;
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             p.runGen();
             score = p.getChromosomes().get(p.getChromosomes().size() - 1).calcCost();
             //System.out.println(score);
 
         }
         System.out.println(score);
+        System.out.println();
+        for (int i = 0; i < 20; i++) {
+            System.out.print(p.getChromosomes().get(p.getChromosomes().size() - 1).getC()[i] + ", ");
+
+        }
 
     }
+
 
 
 }
